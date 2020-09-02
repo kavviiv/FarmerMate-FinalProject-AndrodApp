636ac5f4dcc14e2f3d43e360d4d03d580e3f5a89
kavviiv <jameskoco.24@gmail.com>
2020-09-03 02:41:16 +0700

    3/09/2020 2.40AM

---
+++ app/src/main/java/com/example/farmermate/RecomDetail.java
@@ -0,0 +1,40 @@
+package com.example.farmermate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class RecomDetail extends AppCompatActivity {
+
+    String Name, Description;
+
+    int position = 0  ;
+    private List<Rice> mRiceList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recom_detail);
+
+        Intent intent = getIntent();
+        position = intent.getExtras().getInt("Position");
+
+        final RiceAdapter adapter = new RiceAdapter(this);
+        final ImageView imgV = (ImageView) findViewById(R.id.mainimg);
+        TextView tvName = (TextView) findViewById(R.id.Name);
+        TextView tvDescription = (TextView) findViewById(R.id.Description);
+
+        imgV.setImageResource(R.drawable.riceim2);
+//        tvName.setText(RiceAdapter.get(position).getName());
+//        tvDescription.setText(RiceAdapter.get(position).getDescription());
+
+    }
+
+
+}
